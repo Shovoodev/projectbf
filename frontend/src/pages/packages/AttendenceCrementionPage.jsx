fa4ba6b40f4817fa7ae96331da87499f5528328c
Toukir Islam <shovoodev@gmail.com>
2026-01-14 22:20:35 +0100

    staging for demo

--- frontend/src/pages/packages/AttendenceCrementionPage.jsx
+++ frontend/src/pages/packages/AttendenceCrementionPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import { List } from "../../components/common/Reusables";
 import { useServiceApi } from "../../utility/SelectedServiceProvider";
