2c4864b7d0af24a3c4d56a600564f86773779cf4
mdathikhasan <mdathikhasan136@gmail.com>
2026-01-26 14:33:37 +0600

    google-review-added

--- frontend/src/pages/Testimonials.jsx
+++ frontend/src/pages/Testimonials.jsx
@@ -1,4 +1,5 @@
 import Hero from "../components/common/Hero";
+import GoogleReviews from "../components/pages/Testimonials/GoogleREview";
 import TestimonialsGridSection from "../components/pages/Testimonials/TestimonialsGridSection";
 
 function Testimonials() {
@@ -6,6 +7,7 @@ function Testimonials() {
     <>
       <Hero title={"Testimonials"} subtitle={"Testimonials"} />
       <section className="max-w-[1360px] mx-auto px-2.5 sm:px-4 lg:px-8 py-12 space-y-16">
+        <GoogleReviews />
         <TestimonialsGridSection />
       </section>
     </>
