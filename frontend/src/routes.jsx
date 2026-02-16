1c0195edf654a7ff05c60827a0fbd6e95b57210b
Toukir Islam <shovoodev@gmail.com>
2026-02-16 18:06:30 +0100

    cancel the router protection

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -20,21 +20,24 @@ import Resources from "./pages/Resources";
 import ServiceArea from "./pages/Service-Area";
 import Team from "./pages/Team";
 import Testimonials from "./pages/Testimonials";
-
+// from Toukirul vai
 import BlankLayout from "./BlankLayout";
+import AgreementForm from "./pages/Agreement";
 
+// import Pay_now from "./pages/Pay_now";
 import AttendenceCrementionPage from "./pages/packages/AttendenceCrementionPage";
 import Registrarion from "./pages/Registration";
 import LoginScreen from "./pages/SignIn";
-
-import BlogPage from "./pages/blog/BlogPage";
-import CreateBlog from "./pages/blog/_component/CreateBlog";
-import BlogDetails from "./pages/blog/_component/BlogDetails";
-
+// import PaynowPage from "./pages/packages/PaynowPage";
+import BlogPage from './pages/blog/BlogPage'
+import CreateBlog from './pages/blog/_component/CreateBlog'
+import BlogDetails from './pages/blog/_component/BlogDetails'
 import NoServiceCrementionPage from "./pages/packages/NoServiceCrementionPage";
+// import PaynowPage from "./pages/packages/PaynowPage";
 import ViewingAndCrementionPage from "./pages/packages/ViewingAndCremention";
 import PrePayindex from "./pages/prepay";
 import Signature from "./pages/Signature";
+// import BlankLayout from "./BlankLayout";
 import AgreementFormPage from "./pages/packages/Agreement";
 import Packages from "./pages/Packages";
 
@@ -42,89 +45,175 @@ import Registration from "./pages/Registration";
 import InvoicePage from "./pages/pay/InvoicePage";
 import StaticInvoicePDF from "./pages/packages/_components/StaticInvoicePDF";
 import NewsDetails from "./components/pages/News/_components/NewsDetails";
-import EditBlog from "./pages/blog/_component/EditBlog";
-import SignIn from "./pages/admin/SignIn";
-import Register from "./pages/admin/Register";
-import Error from "./pages/Error";
-import Intro_video from "./pages/Intro_video";
-import Attending_Cremation_Landing from "./pages/Attending_Cremation_Landing";
-import RouteGuard from "./utility/RouteGuard";
-
 export const routes = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <RouteGuard>
-        <App />
-      </RouteGuard>
-    ),
+
+    element: <App />,
     children: [
-      { path: "/home", element: <Home /> },
-      { path: "/video", element: <Intro_video /> },
+      {
+        path: "/",
+        element: <Home />,
+        isPrivate: false,
+      },
+      {
+        path: "/team",
+        element: <Team />,
+      },
+      {
+        path: "/news",
+        element: <News />,
+      },
+      {
+        path: "/news/:id",
+        element: <NewsDetails />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/music",
+        element: <MusicSection />,
+      },
+      {
+        path: "/chepels",
+        element: <Chapels />,
+      },
+      {
+        path: "/coffins",
+        element: <Coffins />,
+      },
+      {
+        path: "/live-music",
+        element: <LiveMusic />,
+      },
+      {
+        path: "/resources",
+        element: <Resources />,
+      },
+      {
+        path: "/service-area",
+        element: <ServiceArea />,
+      },
+      {
+        path: "/landing-page",
+        element: <LandingPage />,
+      },
+      {
+        path: "/faq",
+        element: <Faq />,
+      },
+      {
+        path: "/testimonials",
+        element: <Testimonials />,
+      },
+      {
+        path: "/refund-policy",
+        element: <Refund_Policy />,
+      },
+      {
+        path: "/disclaimer",
+        element: <Disclaimer />,
+      },
+      {
+        path: "/ash-storage",
+        element: <Ash_Storage />,
+      },
+      {
+        path: "/expansion",
+        element: <Expansion />,
+      },
+      {
+        path: "/packages",
+        element: <Packages />,
+      },
+      {
+        path: "/agreement",
+        element: <AgreementForm />,
+      },
+      {
+        path: "/about-btf",
+        element: <Btf />,
+      },
+      {
+        path: "/signature",
+        element: <Signature />,
+      },
+      // from Toukirul
+      {
+        path: "/packages/attending-service-cremation",
+        element: <AttendenceCrementionPage />,
+      },
+      {
+        path: "/packages/no-service-cremention",
+        element: <NoServiceCrementionPage />,
+      },
+      {
+        path: "/packages/viewing-and-cremention",
+        element: <ViewingAndCrementionPage />,
+      },
+      {
+        path: "/login",
+        element: <LoginScreen />,
+      },
 
-      { path: "/team", element: <Team /> },
-      { path: "/news", element: <News /> },
-      { path: "/news/:id", element: <NewsDetails /> },
-      { path: "/contact", element: <Contact /> },
-      { path: "/music", element: <MusicSection /> },
-      { path: "/chepels", element: <Chapels /> },
-      { path: "/coffins", element: <Coffins /> },
-      { path: "/live-music", element: <LiveMusic /> },
-      { path: "/resources", element: <Resources /> },
-      { path: "/service-area", element: <ServiceArea /> },
-      { path: "/landing-page", element: <LandingPage /> },
-      { path: "/faq", element: <Faq /> },
-      { path: "/testimonials", element: <Testimonials /> },
-      { path: "/refund-policy", element: <Refund_Policy /> },
-      { path: "/disclaimer", element: <Disclaimer /> },
-      { path: "/ash-storage", element: <Ash_Storage /> },
-      { path: "/expansion", element: <Expansion /> },
-      { path: "/packages", element: <Packages /> },
-      { path: "/about-btf", element: <Btf /> },
-      { path: "/signature", element: <Signature /> },
-
-      { path: "/packages/attending-service-cremation", element: <AttendenceCrementionPage /> },
-      { path: "/packages/no-service-cremention", element: <NoServiceCrementionPage /> },
-      { path: "/packages/viewing-and-cremention", element: <ViewingAndCrementionPage /> },
-
-      { path: "/login", element: <LoginScreen /> },
-      { path: "/packages/standard/register", element: <Registrarion /> },
-      { path: "/register", element: <Registration /> },
-      { path: "/packages/basic/register", element: <Registrarion /> },
+      {
+        path: "/packages/standard/register",
+        element: <Registrarion />,
+      },
+      {
+        path: "/register",
+        element: <Registration />,
+      },
+      {
+        path: "/packages/basic/register",
+        element: <Registrarion />,
+      },
+      // {
+      //   path: "/:totalPrice/packages/basic/prepage",
+      //   element: <PaynowPage />,
+      // },
 
       {
         path: "/fill-agreement-form",
         element: <AgreementFormPage />,
-        handle: { requiresAuth: true },
+        isPrivate: true,
+      },
+      {
+        path: "/blog",
+        element: <BlogPage />,
+        isPrivate: false,
+      },
+      {
+        path: "/create-new-blog",
+        element: <CreateBlog />,
+        isPrivate: false,
+      },
+      {
+        path: "/blog/:id",
+        element: <BlogDetails />,
       },
       {
         path: "/pay-now",
         element: <InvoicePage />,
-        handle: { requiresAuth: true },
+        isPrivate: true,
       },
-
-      { path: "/blog", element: <BlogPage /> },
-      { path: "/edit-blog/:id", element: <EditBlog /> },
-      { path: "/create-new-blog", element: <CreateBlog /> },
-      { path: "/blog/:id", element: <BlogDetails /> },
-
-      { path: "/attending-cremation-landing", element: <Attending_Cremation_Landing /> },
-
-      { path: "*", element: <Error /> },
     ],
   },
-
   {
-    element: (
-      <RouteGuard>
-        <BlankLayout />
-      </RouteGuard>
-    ),
+    element: <BlankLayout />,
     children: [
-      { path: "/prepay", element: <PrePayindex />, handle: { requiresAuth: true } },
-      { path: "/invoicePdf", element: <StaticInvoicePDF />, handle: { requiresAuth: true } },
-      { path: "/administration-btf", element: <SignIn />, handle: { requiresAuth: true } },
-      { path: "/add-new-btf-admin", element: <Register />, handle: { requiresAuth: true } },
+      {
+        path: "/prepay",
+        element: <PrePayindex />,
+        isPrivate: true,
+      },
+      {
+        path: "/invoicePdf",
+        element: <StaticInvoicePDF />,
+        isPrivate: true,
+      },
     ],
   },
 ]);
--- frontend/src/utility/RouteGuard.jsx
+++ frontend/src/utility/RouteGuard.jsx
@@ -1,15 +1,13 @@
 import { Navigate, useLocation, useMatches } from "react-router-dom";
-import { useUserFront } from "./use-userFront";
 
 export default function RouteGuard({ children }) {
-    const { user } = useUserFront();
     const location = useLocation();
     const matches = useMatches();
 
     // true if any matched route in the branch requires auth
     const requiresAuth = matches.some((m) => m.handle?.requiresAuth);
 
-    if (requiresAuth && !user?._id) {
+    if (requiresAuth) {
         return <Navigate to="/login" replace state={{ from: location.pathname }} />;
     }
 
